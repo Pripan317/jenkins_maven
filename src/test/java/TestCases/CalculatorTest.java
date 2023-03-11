1fe79cf91c13a3407172cdec243469e0b0fcbe79
princebxr02@gmail.com <princebxr02@gmail.com>
2023-03-11 23:37:58 +0530

    maven_jenkins

---
+++ src/test/java/TestCases/CalculatorTest.java
@@ -0,0 +1,56 @@
+package TestCases;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Jprince.Calculator;
+
+public class CalculatorTest {
+	public static Calculator objscientificCalculator;
+	@Before
+	public void setUp() throws Exception {
+		objscientificCalculator=new Calculator();
+		
+		System.out.println("createing obj for Class under test");
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("closing the Test execjtion");
+	}
+
+	@Test
+	public void testGetwishes() {
+		
+		String result=objscientificCalculator.getwishes();
+		assertEquals("TC_GetWishes ","Hello world",result);
+		
+	}
+
+	@Test
+	public void testAddition() {
+		int varResult_addition=objscientificCalculator.addition(12, 12);
+		assertEquals("TC_Addition", 24,varResult_addition);
+	}
+
+	@Test
+	public void testSubstraction() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testMultiplication() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testDivision() {
+		assertTrue(true);
+	}
+
+}
